55d40e6d5899a1b05cd02f369449a3456e41c674
aptsemi <aptsemi@protonmail.com>
2022-11-18 14:39:58 -0800

    descriptors

--- fizzbuzz/models.py
+++ fizzbuzz/models.py
@@ -4,3 +4,5 @@ class Fizzbuzz(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
 
+    def __str__(self):
+        return self.name + ' '  + self.description
