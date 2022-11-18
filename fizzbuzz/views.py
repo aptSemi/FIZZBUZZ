3d3d56984d54ec03e64edbe27f30daa8889e5d60
aptsemi <aptsemi@protonmail.com>
2022-11-18 15:12:37 -0800

    endpoints

---
+++ fizzbuzz/views.py
@@ -0,0 +1,14 @@
+from django.http import JsonResponse
+from .models import Fizzbuzz
+from .serializers import FizzbuzzSerializer
+
+def fizzbuzz_list(request):
+
+    #get all the fizzbuzz!
+    #serialize the fizzy bubbly!
+    #return json.....fizzy bubbly
+    
+    fizzbuzz = Fizzbuzz.Objects.all()
+    serializer = FizzbuzzSerializer(fizzbuzz, many=True) 
+    return JsonResponse(Serializer.data)
+
