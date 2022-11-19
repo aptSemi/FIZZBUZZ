a0961fe47bb6951cdd09c734f74192d6492bbf36
aptsemi <aptsemi@protonmail.com>
2022-11-19 14:01:50 -0800

    added POST

--- fizzbuzz/views.py
+++ fizzbuzz/views.py
@@ -1,10 +1,20 @@
 from django.http import JsonResponse
 from .models import Fizzbuzz
 from .serializers import FizzbuzzSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
 def fizzbuzz_list(request):
 
-    fizzbuzz = Fizzbuzz.objects.all()
-    serializer = FizzbuzzSerializer(fizzbuzz, many=True) 
-    return JsonResponse({'fizzbuzz': serializer.data})
+    if request.method == 'GET':
+        fizzbuzz = Fizzbuzz.objects.all()
+        serializer = FizzbuzzSerializer(fizzbuzz, many=True) 
+        return JsonResponse({'fizzbuzz': serializer.data})
 
+    if request.method == 'POST':
+        serializer = FizzbuzzSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
