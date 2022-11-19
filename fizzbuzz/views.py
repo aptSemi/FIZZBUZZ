d66845cf59e5f8c22fea7017af4d4c7f09d7f812
aptsemi <aptsemi@protonmail.com>
2022-11-18 16:10:03 -0800

    fixed response data

--- fizzbuzz/views.py
+++ fizzbuzz/views.py
@@ -4,11 +4,7 @@ from .serializers import FizzbuzzSerializer
 
 def fizzbuzz_list(request):
 
-    #get all the fizzbuzz!
-    #serialize the fizzy bubbly!
-    #return json.....fizzy bubbly
-    
     fizzbuzz = Fizzbuzz.objects.all()
     serializer = FizzbuzzSerializer(fizzbuzz, many=True) 
-    return JsonResponse(serializer.data, safe=False)
+    return JsonResponse({"fizzbuzz": serializer.data}, safe=False)
 
